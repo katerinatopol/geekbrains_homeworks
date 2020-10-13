9a5a1290fcf208fad229b3bb7223c4d343a0bf68
katerina_topol <katerina_topol@mail.ru>
2020-10-13 18:47:59 +0300

    add 3.3.py

---
+++ 3.3.py
@@ -0,0 +1,14 @@
+"""
+3. Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов.
+"""
+
+
+def my_func(var_1, var_2, var_3):
+    my_list = [var_1, var_2, var_3]
+    my_list.remove(min(var_1, var_2, var_3))
+    return sum(my_list)
+
+
+print(my_func(100, 7, 200))
+
+
